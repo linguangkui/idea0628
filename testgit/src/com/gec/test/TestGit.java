f70cef3aab8819aaff35ddf0e8342c611f141223
linguangkui <linguangkui@123.com>
2020-08-14 18:41:10 +0800

    idea第二次上传

--- testgit/src/com/gec/test/TestGit.java
+++ testgit/src/com/gec/test/TestGit.java
@@ -4,4 +4,8 @@ public class TestGit {
     public void addUser(){
         System.out.println("public void addUser()...");
     }
+
+    public void updateUser(){
+        System.out.println("public void updateUser()");
+    }
 }
