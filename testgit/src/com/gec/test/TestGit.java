da4cf29a7e9c481760ef7e6254515b79ecbf1417
linguangkui <linguangkui@123.com>
2020-08-14 18:59:10 +0800

    分支上传

--- testgit/src/com/gec/test/TestGit.java
+++ testgit/src/com/gec/test/TestGit.java
@@ -1,15 +1,7 @@
 package com.gec.test;
 
 public class TestGit {
-    public void addUser(){
-        System.out.println("public void addUser()...");
-    }
-
-    public void updateUser(){
-        System.out.println("public void updateUser()");
-    }
-
-    public void deleteUser(){
-        System.out.println("public void deleteUser()");
-    }
+   public void selectUser(){
+       System.out.println("public void selectUser()");
+   }
 }
