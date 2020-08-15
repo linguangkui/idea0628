7160b31a92a0cf42732281a41f34b351c0aa19a3
linguangkui <linguangkui@123.com>
2020-08-15 09:51:53 +0800

    branch提交

--- testgit/src/com/gec/test/TestGit.java
+++ testgit/src/com/gec/test/TestGit.java
@@ -1,8 +1,8 @@
 package com.gec.test;
 
 public class TestGit {
-    public void test(){
-        System.out.println("分支添加了代码");
+    public void test1(){
+        System.out.println("分支添加了代码888888");
     }
     public void test2(){
         System.out.println("分支添加了代码");
