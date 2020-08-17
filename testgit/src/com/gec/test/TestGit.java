7d4683e4ce21eed7ed2e04e73386aab25b0b6636
linguangkui <linguangkui@163.com>
2020-08-17 19:21:39 +0800

    eclipse添加了代码

--- testgit/src/com/gec/test/TestGit.java
+++ testgit/src/com/gec/test/TestGit.java
@@ -2,12 +2,12 @@ package com.gec.test;
 
 public class TestGit {
     public void test1(){
-        System.out.println("分支添加了代码888888");
+        System.out.println("添加了代码888888");
     }
     public void test2(){
-        System.out.println("分支添加了代码");
+        System.out.println("添加了代码");
     }
     public void test3(){
-        System.out.println("分支添加了代码");
+        System.out.println("添加了代码");
     }
 }
